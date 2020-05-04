611a2bf90cea8853c75a2ee0ea9d314424e4e3bb
rguptavh <rguptavh119@gmail.com>
2020-05-03 23:26:08 -0500

    login screen change

--- components/Login.js
+++ components/Login.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, StyleSheet, TouchableWithoutFeedback, Keyboard, TextInput, Image, ImageBackground, TouchableOpacity, Alert, Dimensions, AsyncStorage } from 'react-native';
+import { View, StyleSheet, Text, TouchableWithoutFeedback, Keyboard, TextInput, Image, ImageBackground, TouchableOpacity, Alert, Dimensions, AsyncStorage, KeyboardAvoidingView } from 'react-native';
 import moment from 'moment';
 import Spinner from 'react-native-loading-spinner-overlay';
 
@@ -38,36 +38,36 @@ export default class Login extends React.Component {
         ok = Http.responseText;
         if (Http.readyState == 4) {
           console.log(String(ok));
-          
-          if (ok.substring(0,4) == "true"){
+
+          if (ok.substring(0, 4) == "true") {
             // console.log(response.toString());
             global.uname = uname;
-            var total = parseFloat(ok.substring(5,ok.indexOf(",",5)));
+            var total = parseFloat(ok.substring(5, ok.indexOf(",", 5)));
             global.hours = Math.floor(total);
-            global.minutes = Math.round((total-global.hours)*60);
+            global.minutes = Math.round((total - global.hours) * 60);
             console.log(global.minutes)
-            var data = JSON.parse(ok.substring(ok.indexOf(",",5)+1,ok.length))
-            
+            var data = JSON.parse(ok.substring(ok.indexOf(",", 5) + 1, ok.length))
+
             // console.log(JSON.stringify(data))
             var ongoing = [];
             var specific = [];
             var log = [];
-            for (var x=0; x<data.length;x++){
-              if (data[x].type == "Log"){
-                data[x]["id"] = ""+x;
+            for (var x = 0; x < data.length; x++) {
+              if (data[x].type == "Log") {
+                data[x]["id"] = "" + x;
                 log.push(data[x]);
               }
-              else if (data[x].type == "Ongoing"){
-                data[x]["id"] = ""+x;
+              else if (data[x].type == "Ongoing") {
+                data[x]["id"] = "" + x;
                 ongoing.push(data[x]);
               }
-              else if (data[x].type == "Specific"){
-                data[x]["id"] = ""+x;
+              else if (data[x].type == "Specific") {
+                data[x]["id"] = "" + x;
                 specific.push(data[x]);
               }
             }
             console.log(data)
-            
+
             specific = specific.sort((a, b) => moment(a.date + " " + a.start, 'MM-DD-YYYY h:mm A').format('X') - moment(b.date + " " + b.start, 'MM-DD-YYYY h:mm A').format('X'))
             log = log.sort((a, b) => moment(b.date, 'MM-DD-YYYY').format('X') - moment(a.date, 'MM-DD-YYYY').format('X'))
             const map = new Map();
@@ -84,13 +84,20 @@ export default class Login extends React.Component {
                 // console.log(result)
                 const he = {
                   header: true,
-                  id: "" + (data.length+i),
+                  id: "" + (data.length + i),
                   date: log[i].date
                 }
                 log.splice(i, 0, he);
               }
             }
-
+            options = []
+            for (const item of ongoing) {
+              options.push({ label: item.name, value: item.name })
+            }
+            for (const item of specific) {
+              options.push({ label: item.name, value: item.name })
+            }
+            global.options = options;
             global.ongoing = ongoing;
             global.specific = specific;
             global.logs = log;
@@ -99,20 +106,22 @@ export default class Login extends React.Component {
             this.props.navigation.replace('Main')
 
           }
-          else if (ok.substring(0,5) == "false") {
+          else if (ok.substring(0, 5) == "false") {
             this.setState({ loading: false });
-            setTimeout(() => {  alert("Failed Login"); }, 100);
+            setTimeout(() => { alert("Failed Login"); }, 100);
 
           }
           else {
             this.setState({ loading: false });
-            setTimeout(() => {  alert("Server Error"); }, 100);
+            setTimeout(() => { alert("Server Error"); }, 100);
           }
 
         }
       }
     }
     return (
+      <KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"}
+      style={styles.container}>
       <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()} accessible={false}>
 
         <View style={styles.container}>
@@ -123,47 +132,71 @@ export default class Login extends React.Component {
           />
           <ImageBackground source={require('../assets/login.png')} style={styles.image}>
 
-          <View style = {{flex:3, width: '100%', alignItems:'center'}}>
-            <Image source={require('../assets/vh.png')} style={styles.imagefront} resizeMode="contain"></Image>
+            <View style={{ flex: 3, width: '100%', alignItems: 'center' }}>
+              <Image source={require('../assets/vh.png')} style={styles.imagefront} resizeMode="contain"></Image>
             </View>
-            <ImageBackground source={require('../assets/form.png')} style={{
+            <View style={{
               alignItems: 'center',
-              flex: 4,
-              width: '100%',
+              flex: 2,
+              width: '90%',
+              backgroundColor: "#D1DAE7",
+              borderRadius: 20,
+              justifyContent:'center',
+              shadowOffset: {
+                width: 0,
+                height: 4,
+              },
+              shadowOpacity: 0.30,
+              shadowRadius: 4.65,
+          
+              elevation: 8,
+            }}>
+              <View style = {{width:'100%',height:'85%', alignItems:'center'}}>
+                <View style={{ flex: 1, width: '90%', justifyContent: 'center'}}>
+                  <Text style={{ fontFamily: 'Noto', color:'#3C5984', fontSize:rem*15 }}>Username</Text>
+                </View>
+                <View style={{
+                  width: '90%',
+                  flex: 1.5,
+                  borderColor:'#3C5984',
+                  borderWidth:2,
+                  borderRadius:15
+                }}>
+                  <TextInput
+                    style={{ fontSize: 15 * rem, width:'100%',height:'100%' }}
+                    textAlign={'center'}
+                    autoCapitalize='none'
+                    autoCompleteType='off'
+                    onChangeText={(value) => this.setState({ password: value })}
+                    value={this.state.password}
 
-            }} resizeMode="contain">
-              <View style={{
-                width: 200 * wid,
-                flex: 1,
-                justifyContent: 'flex-end'
-              }}>
-                <TextInput
-                  style={{ fontSize: 12 * rem, width: 200 * wid, marginBottom: 24 * ree, }}
-                  textAlign={'center'}
-                  autoCapitalize='none'
-                  autoCompleteType='off'
-                  onChangeText={(value) => this.setState({ username: value })}
-                  value={this.state.username}
+                  /></View>
+                  <View style = {{width:'100%',flex:0.4}}></View>
+                <View style={{ flex: 1, width: '90%', justifyContent: 'center'}}>
+                  <Text style={{ fontFamily: 'Noto', color:'#3C5984', fontSize:rem*15 }}>Password</Text>
+                </View>
+                <View style={{
+                  width: '90%',
+                  flex: 1.5,
+                  borderColor:'#3C5984',
+                  borderWidth:2,
+                  borderRadius:15
+                }}>
+                  <TextInput
+                    style={{ fontSize: 15 * rem, width:'100%',height:'100%' }}
+                    textAlign={'center'}
+                    autoCapitalize='none'
+                    autoCompleteType='off'
+                    onChangeText={(value) => this.setState({ username: value })}
+                    value={this.state.username}
+                    secureTextEntry={true}
 
-                /></View>
-              <View style={{
-                width: 200 * wid,
-                flex: 1,
-
-              }}>
-                <TextInput
-                  style={{ fontSize: 12 * rem, width: 200 * wid, marginTop: ree * 37 }}
-                  textAlign={'center'}
-                  onChangeText={(value) => this.setState({ password: value })}
-                  value={this.state.password}
-                  secureTextEntry={true}
-                />
-
+                  /></View>
               </View>
-            </ImageBackground>
+            </View>
             <View style={{
               width: '73%',
-              flex: 2,
+              flex: 1,
               justifyContent: 'center'
             }}>
               <TouchableOpacity
@@ -180,7 +213,6 @@ export default class Login extends React.Component {
                   width: '100%',
                   flex: 1
 
-
                 }} resizeMode="contain"></Image>
               </TouchableOpacity>
             </View>
@@ -188,6 +220,7 @@ export default class Login extends React.Component {
           </ImageBackground>
         </View>
       </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -198,7 +231,7 @@ const styles = StyleSheet.create({
     height: Dimensions.get('window').height,
     justifyContent: 'center',
     alignItems: 'center',
-    left: 0, top: 0, position: 'absolute'
+   // left: 0, top: 0, position: 'absolute'
 
   },
   image: {
@@ -210,7 +243,7 @@ const styles = StyleSheet.create({
   },
   imagefront: {
     marginTop: '12%',
-    paddingTop:'5%',
+    paddingTop: '5%',
     height: '25%',
     width: '80%',
     flex: 3,
